97a23e8619f1ab6982e5c8d2871175520293b252
Anna Alemany <annalemany@gmail.com>
2021-08-23 17:29:39 +0200

    correct samse line: folder for fq file was missing

--- mapping/ribo-bwamem.sh
+++ mapping/ribo-bwamem.sh
@@ -31,7 +31,7 @@ fq=${fq#*/}
 
 # mapping short reads
 ${p2bwa}/bwa aln ${ref} ${folder}/${fq} > ${folder}/aln_${fq%.f*q}.sai 
-${p2bwa}/bwa samse  ${ref}  ${folder}/aln_${fq%.f*q}.sai ${fq} | ${p2samtools}/samtools view -Sb > ${out}.aln-ribo.bam &
+${p2bwa}/bwa samse ${ref} ${folder}/aln_${fq%.f*q}.sai ${folder}/${fq} | ${p2samtools}/samtools view -Sb > ${out}.aln-ribo.bam &
 
 # mapping normal reads
 ${p2bwa}/bwa mem -t 8 -h 15 ${ref} ${folder}/${fq} | ${p2samtools}/samtools view -Sb > ${out}.mem-ribo.bam & 
